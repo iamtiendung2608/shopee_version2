dbc6263a0b69649bd16b7073a2335a1e813cd1f5
Tien Dung <ptdung2608@gmail.com>
2021-07-06 00:09:55 +0700

    add Item and Interface

--- Item.java
+++ Item.java
@@ -1,3 +1,44 @@
+import java.util.ArrayList;
 public class Item{
-    
+    ArrayList<String>ListBuy;
+    static int count=0;
+    static int Price=0;
+    Item(){
+        ListBuy=new ArrayList<>();
+    }
+    public void getApple(){
+        count++;
+        Price+=23000;
+        ListBuy.add("Apple");
+    }
+    public void getBanana(){
+        count++;
+        Price+=24000;
+        ListBuy.add("Banana");
+    }
+    public void getCherry(){
+        count++;
+        Price+=50000;
+        ListBuy.add("Cherry");
+    }
+    public void getStrawBerry(){
+        count++;
+        Price+=70000;
+        ListBuy.add("StrawBerry");
+    }
+    public void getVocado(){
+        count++;
+        Price+=30000;
+        ListBuy.add("StrawBerry");
+    }
+    public void reset(){
+        count=0;
+        Price=0;
+        ListBuy.clear();
+    }
+    public int getPrice(){return Price;}
+    public int getCount(){return count;}
+    public ArrayList<String> getList(){
+        return ListBuy;
+    }
 }
--- Shopee_Interface.java
+++ Shopee_Interface.java
@@ -11,6 +11,8 @@ public class Shopee_Interface {
     JButton Enter;
     JPanel mainPanel;
     JLabel OutLabel;
+    JButton Buy;
+    static int Num=0;
     public static void main(String[] args) {
         Shopee_Interface gui=new Shopee_Interface();
         gui.go();
@@ -18,29 +20,26 @@ public class Shopee_Interface {
     void go(){
         frame=new JFrame("Shopee");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel Label=new JLabel("Enter your name: ");
         mainPanel=new JPanel();
-        Font bigFont=new Font("serif",Font.BOLD,24);
         Enter=new JButton("Click here");
-        OutLabel=new JLabel("");
+        OutLabel=new JLabel("Enter your name: ");
         Enter.addActionListener(new NameListener());
 
-        name=new JTextField(10);
+        name=new JTextField(5);
         name.setText("");
         name.requestFocus();
-        // add listener
-       
         Male=new JCheckBox("Male");
         Female=new JCheckBox("Female");
-        // add listener
-        mainPanel.add(Label);
+        MyDrawImage image=new MyDrawImage();
+
         mainPanel.add(name);
         mainPanel.add(Male);
         mainPanel.add(Female);
         mainPanel.add(Enter);
         mainPanel.add(OutLabel);
-        frame.getContentPane().add(mainPanel);
-        frame.setSize(500,500);
+        frame.getContentPane().add(BorderLayout.NORTH,mainPanel);
+        frame.getContentPane().add(image);
+        frame.setSize(800,800);
         frame.setVisible(true);
     }
     public class NameListener implements ActionListener{
@@ -52,7 +51,49 @@ public class Shopee_Interface {
             if(Female.isSelected()){
                 Female.setSelected(false);  
                 OutLabel.setText("Hello Mrs."+name.getText()+" Welcome to shopee");
-            }              
+            }
+            name.setVisible(false);
+            Male.setVisible(false);
+            Female.setVisible(false);
+            Enter.setVisible(false);
+            Buy=new JButton("Start Shopping");
+            mainPanel.add(Buy);
+            Buy.addActionListener(new BuyingListener());
+        }
+    }
+    public class BuyingListener implements ActionListener{
+        public void actionPerformed(ActionEvent ev){
+            Buy.setVisible(false);
+            JButton BBuy=new JButton("Buy");
+            JButton NBuy=new JButton("Don't Buy");
+            mainPanel.add(BBuy);
+            mainPanel.add(NBuy);
+        }
+    }
+
+    class MyDrawImage extends JPanel{
+        public void paintComponent(Graphics  g){
+            Image image;
+            if(Num==0){
+                image=new ImageIcon("Shopee.jpg").getImage();
+                g.drawImage(image, 0, 100, this);
+            }
+            if(Num==1){
+                image=new ImageIcon("cherry.jpg").getImage();
+                    g.drawImage(image, 3, 4, this);
+            }
+            if(Num==2){
+                image=new ImageIcon("cherry.jpg").getImage();
+                    g.drawImage(image, 3, 4, this);
+            }
+            if(Num==3){
+                image=new ImageIcon("cherry.jpg").getImage();
+                    g.drawImage(image, 3,4, this);
+            }
+            if(Num==4){
+                image=new ImageIcon("cherry.jpg").getImage();
+                    g.drawImage(image, 3,4, this);
+            }
         }
     }
 }
